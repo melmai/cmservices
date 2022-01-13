2c8172b81e8243cf838de2378647a4e137b9af01
Mel <mel@mel.codes>
2022-01-13 14:59:58 -0800

    updates portfolio template with ACF

--- functions.php
+++ functions.php
@@ -70,3 +70,35 @@ function cms_body_classes( $classes ) {
     
 }
 add_filter( 'body_class','cms_body_classes' );
+
+/**
+ * Adds ACF to single portfolio details
+ * 
+ */
+add_action( 'fusion_after_portfolio_side_content', 'cms_add_fields_to_single_portfolio_post' );
+function cms_add_fields_to_single_portfolio_post() {
+  $association = get_field( 'association' );
+
+  if ( $association != '' ) : ?>
+    <?php $association_link = get_field( '$association_link' ); ?>
+
+    <div class="project-info-box">
+			<h4>Association:</h4>
+      <div class="project-terms"><a href="<?php echo $association_link; ?>" target="_blank"><?php echo $association; ?><a></div>
+		</div>
+	<?php endif; 
+}
+
+add_action( 'fusion_before_additional_portfolio_content', 'cms_add_content_to_single_portfolio_post' );
+function cms_add_content_to_single_portfolio_post() {
+  $has_link = get_field( 'has_additional_info' );
+
+ if ( $has_link ) : ?>
+    <?php $link = get_field( 'link' ); ?>
+    
+		<div class="project-details-link">
+			<a href="<?php echo $link; ?>" target="_blank">See Additional Information <span class="ext-link-icon"><i class="fas fa-external-link-alt"></i></span></a>
+		</div>
+	<?php 
+  endif;
+}
